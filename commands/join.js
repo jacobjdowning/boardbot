2303f53822ffa2498f07a568d12639ca54f02d45
Jacob <jacobjdowning@gmail.com>
2020-04-25 15:26:55 -0400

    implement multi-table support

--- commands/join.js
+++ commands/join.js
@@ -1,7 +1,12 @@
 module.exports = {
     "name": "join",
     "execute": (msg, args, table) =>{
-        table.addPlayer(msg.author.id, msg.author.username, args[0]);
-        msg.reply("Added Player");
+        if(table.addPlayer(msg.author.id, msg.author.username, args[0])){
+            msg.reply("Added Player");
+        }else{
+            msg.reply("you are currently sitting at a different table\n" +
+            "Please run !remove to leave your current table before joining "+
+            "this one");
+        }
     }
 }
