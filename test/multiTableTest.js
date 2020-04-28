bf53219f9075f89b993356be00740a942c4ebb71
Jacob <jacobjdowning@gmail.com>
2020-04-28 13:59:19 -0400

    Add test for multiple tables

---
+++ test/multiTableTest.js
@@ -0,0 +1,75 @@
+//A list of messages to be sent in order
+module.exports = [
+    {
+        "content":"!join",
+        "channel":{
+            "id" : 0
+        },
+        "author":{
+            "id": 0
+        }
+    },
+    {
+        "content":"!join",
+        "channel":{
+            "id" : 0
+        },
+        "author":{
+            "id": 1
+        }
+    },
+    {
+        "content":"!join",
+        "channel":{
+            "id" : 1
+        },
+        "author":{
+            "id": 2
+        }
+    },
+    {
+        "content":"!codenames",
+        "channel":{
+            "id" : 0
+        },
+        "author":{
+            "id": 0
+        }
+    },
+    {
+        "content":"!join",
+        "channel":{
+            "id" : 1
+        },
+        "author":{
+            "id": 3
+        }
+    },   
+    {
+        "content":"!codenames",
+        "channel":{
+            "id" : 1
+        },
+        "author":{
+            "id": 2
+        }
+    },
+    {
+        "content":"!list",
+        "channel":{
+            "id" : 1
+        },
+        "author":{
+            "id":2
+        }
+    },
+    {
+        "content":"!clear",
+        "channel":{
+            "id" : 1
+        },
+        "author":{
+            "id":2
+        }
+    },
+]
