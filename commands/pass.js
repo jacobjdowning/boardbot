0e8bcaa94c357541d7375be78a365e370308936e
Jacob <jacobjdowning@gmail.com>
2020-05-01 13:55:18 -0400

    implemented collectors for avalon commands

--- commands/pass.js
+++
@@ -1,8 +0,0 @@
-const vote = require('./common/vote.js');
-module.exports = {
-    "name": "pass",
-    "gameonly": "avalon",
-    "execute": (msg, args, table) => {
-        vote(true, table.game, msg.author.id, msg, table);
-    }
-}
