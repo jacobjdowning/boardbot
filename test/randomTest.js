edae382efab87d61a2b665243e208e114b29145e
Jacob <jacobjdowning@gmail.com>
2020-06-13 14:53:11 -0400

    implement randomize command

---
+++ test/randomTest.js
@@ -0,0 +1,57 @@
+//A list of messages to be sent in order
+module.exports = [
+    {
+        "content":"!join",
+        "channel":{
+            "id" : 0
+        },
+        "author":{
+            "id": 0
+        }
+    },
+    {
+        "content":"!join black",
+        "channel":{
+            "id" : 0
+        },
+        "author":{
+            "id": 1
+        }
+    },
+    {
+        "content":"!join white",
+        "channel":{
+            "id" : 0
+        },
+        "author":{
+            "id": 2
+        }
+    },
+    {
+        "content":"!join white",
+        "channel":{
+            "id" : 0
+        },
+        "author":{
+            "id": 3
+        }
+    },
+    {
+        "content":"!randomize black white",
+        "channel":{
+            "id" : 0
+        },
+        "author":{
+            "id": 0
+        }
+    },
+    {
+        "content":"!list",
+        "channel":{
+            "id" : 0
+        },
+        "author":{
+            "id": 0
+        }
+    },
+]
