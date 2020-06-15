a0cee4cba8e260d55e2f59d28961415633901d0a
Jacob <jacobjdowning@gmail.com>
2020-06-15 17:05:12 -0400

    most of the roles finished

--- commands/dracula.js
+++ commands/dracula.js
@@ -10,10 +10,26 @@ async function whisperVote(player){
     return collection.first().content.includes('yes');
 }
 
+function zombieNeighbours(table, zombie) {
+    const players = table.teams.get('unteamed');
+    const zombieIndex = players.findIndex(player => player.id == zombie.id);
+    let zombieNeighbourIndex = [zombieIndex + 1, zombieIndex - 1];
+    for (let i = 0; i < zombieNeighbourIndex.length; i++) {
+        if(zombieNeighbourIndex[i] < 0){
+            zombieNeighbourIndex[i] = players.length-1;
+        }else if(zombieNeighbourIndex[i] >= players.length){
+            zombieNeighbourIndex[i] = 0;
+        }
+    }
+    return zombieNeighbourIndex.map(index => players[index]);
+}
+
 module.exports = {
     "name": "dracula",
     "onlyGame": "dracula",
     "execute": (message, args, table) => {
+        const userCache = message.client.users.cache
+
         if(args.some(arg => arg.toLowerCase() == "jekswap")){
             const jekyll = table.game.players.filter(player =>
                     player[0].id = message.author.id
@@ -25,6 +41,13 @@ module.exports = {
             }
 
             //Swap Jekyll
+            const tempJek = jekyll[1];
+            jekyll[1] = table.game.mysteryGuest;
+            table.game.mysteryGuest = tempJek;
+
+            userCache.get(player[0].id).send(
+                `You are now the character ${jekyll[1].name}\n${jekyll[1].desc}`
+            )
 
             return;
         }
@@ -72,11 +95,23 @@ module.exports = {
             ,'').slice(0, -2)
         )
 
-        const cache = message.client.users.cache
-        table.game.players.forEach(player => {
-            cache.get(player[0].id).send(
+        table.game.players.forEach(player  => {
+            let characterSpecific = "";
+            switch (player[1].name) {
+                case "Zombie":
+                    const neighbours = zombieNeighbours(table, player[0]);
+                    characterSpecific = "\nYour neighbours are :"+
+                        `${neighbours[0].name} and ${neighbours[1].name}`
+                    break;
+                case "Magic Mirror":
+                    characterSpecific = `\nThe Mystery Guest `+
+                    `is ${table.game.mysteryGuest.name}`
+                    break;
+            }
+            userCache.get(player[0].id).send(
                 `Role:${player[1].name}\n`+
-                `Description:${player[1].desc}`
+                `Description:${player[1].desc}`+
+                characterSpecific
             )            
         });
     }
