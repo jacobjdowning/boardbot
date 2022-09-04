7fbbca2e618447f521de3072c10f80b64ea8e8d7
Jacob Downing <jacobjdowning@gmail.com>
2022-09-04 12:50:12 -0400

    prompt and category test for fake artist

--- commands/fakeArtist.js
+++ commands/fakeArtist.js
@@ -1,16 +1,16 @@
 module.exports = {
     "name":"fakeartist",
     "execute": (message, args, table) => {
-        console.log("we Got HERE !!!!!")
         if (message.channel.type == 'text'){
+            //Select a car
             //Select a fake artist
-            console.log("We got here")
             const teamsArray = Array.from(table.teams.values())
             const fakesTeam = teamsArray[Math.floor(Math.random()*teamsArray.length)]
             const fakePlayer = fakesTeam[Math.floor(Math.random()*fakesTeam.length)]
             const fakeUser = message.client.users.cache.get(fakePlayer.id);
-            fakeUser.send("You are the Fake Artist!")
             //Send the fake artist a response
+            fakeUser.send("You are the Fake Artist!")
+            //Send the rest of the responses
         }else{
             message.reply("games must be started inside a text channel")
         }
--- test/fakeArtist.test.js
+++ test/fakeArtist.test.js
@@ -1,7 +1,11 @@
 const mockery = require('mockery');
 const mockDiscord = require('./mockDiscord.js');
-const should = require('chai').should();
+const chai = require('chai')
+const should = chai.should();
+const chaiEach = require('chai-each');
+chai.use(chaiEach);
 const { startWithRequests } = require('./testHelpers.js');
+const monikersCards = require('../assets/moinkersCards.json');
 let consolelog;
 
 describe('Fake Artist Goes to NY', function(){
@@ -16,6 +20,9 @@ describe('Fake Artist Goes to NY', function(){
 
     context('Four users, 0u - 3u, join unteamed and u0 enters'+
         ' the !fakeartist command', function () {
+        before(function(){
+            this.numUsers = 4;
+        })
         beforeEach(async function(){
             const app = startWithRequests('./requests/fakeArtistReq.js');
             this.currentTest.app = app;
@@ -25,7 +32,7 @@ describe('Fake Artist Goes to NY', function(){
         it('should have told someone and only one they ' + 
             'were the fake artist', function(){
 
-            console.log(this.test.log)
+            // console.log(this.test.log)
 
             const message="You are the Fake Artist!"
             const totalMatch = this.test.log.reduce((count, log) =>{
@@ -33,6 +40,26 @@ describe('Fake Artist Goes to NY', function(){
             }, 0);
 
             totalMatch.should.equal(1);
+        })
+
+        it('should send all other artists a prompt and a category', function(){
+            //every user that is not the fake artist gets the same prompt
+            const commandLogIndex = this.test.log.find(log=>log.content === '!fakeartist');
+            const artistInforms = this.test.log.slice(commandLogIndex+1);
+            const message="You are the Fake Artist!"
+            const prompts = artistInforms.filter(log => (
+                !(log.content.includes(message))
+            ));
+            prompts.length.should.be.equal(this.numUsers - 1);
+            const promptsConent = prompts.map(log => log.content);
+            promptsContent.should.each.be.equal(promptsConent[0]);
+
+            //the description is a monikers card
+            const splitPrompt = promptsContent[0].slipt(" ");
+            const titleIndex = splitPrompt.findIndex(str => str === "Prompt:");
+            const prompt  = splitPrompt[titleIndex + 1];
+            const card = monikersCards.find(card => card.Person === prompt);
+            card.should.not.be.undefined();
         })
 
         afterEach(function(){
